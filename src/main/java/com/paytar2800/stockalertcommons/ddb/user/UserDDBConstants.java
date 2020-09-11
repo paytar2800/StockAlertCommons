d1ad3070956c1959e315cab0f3167ef829ac47b6
paytar2800 <paytar2800@gmail.com>
2020-09-11 22:29:15 +0530

    Changing the column name shorter

--- src/main/java/com/paytar2800/stockalertcommons/ddb/user/UserDDBConstants.java
+++ src/main/java/com/paytar2800/stockalertcommons/ddb/user/UserDDBConstants.java
@@ -8,7 +8,7 @@ public class UserDDBConstants {
     public static final String TABLE_USERID_KEY = "UserId";
     public static final String TABLE_ALERTSNOOZETIME_KEY = "SnoozeTime";
     public static final String TABLE_ISALERTENABLED_KEY = "AlertEnabled";
-    public static final String TABLE_ISEXTENDEDHOURSENABLED_KEY = "ExtendHourEnabled";
+    public static final String TABLE_ISEXTENDEDHOURSENABLED_KEY = "ExtendHr";
     public static final String TABLE_SUB_STATUS_KEY = "SubStatus";
     public static final String TABLE_DEVICE_TOKEN_KEY = "DeviceToken";
     public static final String TABLE_HAS_CHANGED_KEY = "Changed";
